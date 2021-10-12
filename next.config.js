35a8413cf9809c421475c325a85d3de8f5a9e7c9
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-12 12:40:25 +0530

    Initalizing the app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    images: {
+        domains: [
+            "rb.gy", // shorten url
+            "platform-lookaside.fbsbx.com", // images from facebook
+            "firebasestorage.googleapis.com", //images from firebase
+            "lh3.googleusercontent.com", // images from google
+        ],
+    },
+};
