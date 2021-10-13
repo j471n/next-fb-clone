2296b99e7d3a70f0b239b98c4678202ee38a8fed
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-13 23:04:20 +0530

    Adding Github and google auth

--- components/Login.js
+++ components/Login.js
@@ -1,16 +1,44 @@
-import Image from "next/image";
 import { signIn } from "next-auth/client";
+import Image from "next/image";
 
-export default function Login() {
-    return (
-        <div className="grid place-items-center">
-            <Image
-                src="https://rb.gy/lgowtt"
-                width={400}
-                height={400}
-                objectFit="contain"
-            />
-            <button className="px-7 py-2 bg-blue-500 text-white rounded-md" onClick={signIn}>Login with Facebook</button>
-        </div>
-    );
+export default function Login({ providers }) {
+
+  const { facebook, google, github } = providers;
+  
+  return (
+    <div className="grid place-items-center">
+      <Image
+        src="https://rb.gy/lgowtt"
+        width={400}
+        height={400}
+        objectFit="contain"
+      />
+
+      {facebook && google && (
+        <>
+          <button
+            className="signInButton  lg:hover:bg-blue-500 lg:hover:text-white"
+            onClick={() => signIn(facebook.id)}
+          >
+            <Image src={"https://rb.gy/vsyvzp"} width={30} height={30} />
+            <p className="text-center w-full ">Login with Facebook</p>
+          </button>
+          <button
+            className="signInButton lg:hover:bg-red-500 lg:hover:text-white"
+            onClick={() => signIn(google.id)}
+          >
+            <Image src={"https://rb.gy/dgz5ha"} width={30} height={30} />
+            <p className="text-center w-full">Login with Google</p>
+          </button>
+          <button
+            className="signInButton lg:hover:bg-gray-500 lg:hover:text-white"
+            onClick={() => signIn(github.id)}
+          >
+            <Image src={"https://rb.gy/a45yng"} width={30} height={30} />
+            <p className="text-center w-full">Login with Github</p>
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
