1a4e0d59ad8acc92be37ed554bf13d891186fd3e
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-13 23:03:58 +0530

    updating config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-module.exports = {
-    images: {
-        domains: [
-            "rb.gy", // shorten url
-            "platform-lookaside.fbsbx.com", // images from facebook
-            "firebasestorage.googleapis.com", //images from firebase
-            "lh3.googleusercontent.com", // images from google
-        ],
-    },
-};
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
+  images: {
+    domains: [
+      "rb.gy", // shorten url
+      "platform-lookaside.fbsbx.com", // images from facebook
+      "firebasestorage.googleapis.com", //images from firebase
+      "lh3.googleusercontent.com", // images from google
+      "avatars.githubusercontent.com",
+    ],
+  },
+});
