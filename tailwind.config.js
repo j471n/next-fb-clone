0ce9e5bef1a986dca24bd69003b28f41b26306f3
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-13 22:59:32 +0530

    updating configs and pacakges

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    zIndex: {
+      75: 75,
+      100: 100,
+    },
     extend: {},
   },
   variants: {
