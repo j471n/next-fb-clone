b7b6b95e67ab08d47b5512a76c30c1f5d5db9be3
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-13 23:05:23 +0530

    Adding github and google provides

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,21 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers"
-
+import Providers from "next-auth/providers";
 
 export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-        Providers.Facebook({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        }),
-        Providers.Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET
-        })
-        // ...add more providers here
-    ],
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
 });
