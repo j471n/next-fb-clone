504c38dd7a841c7b2386b354cc426c375e348543
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-13 23:02:01 +0530

    Update Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -4,7 +4,7 @@ import Posts from "./Posts";
 
 export default function Feed({posts}) {
   return (
-    <div className="flex-grow h-screen pb-44 pt-6 xl:mr-40 overflow-y-auto scrollbar-hide">
+    <div className="flex-grow h-screen pb-44 pt-3 md:6 xl:mr-40 overflow-y-auto scrollbar-hide">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <Stories />
         <InputBox />
